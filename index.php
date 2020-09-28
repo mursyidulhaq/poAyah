7980ce736883ca25302c10a7b930ed5edf03e660
mursyid <mursyidulhaq78@gmail.com>
2020-09-28 13:42:01 +0700

    update index

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $api_url = $is_production ?
 
  if (!strpos($_SERVER['REQUEST_URI'],'/change')) {
      http_response_code(404);
-     echo "wrong path, make sure it's '/change'";exit();
+     echo "wrong path, make sure it's '/change'"; exit();
  }
   if($_SERVER['REQUEST_MOTHOD'] !== 'POST'){
     http_response_code(404);
@@ -17,14 +17,14 @@ $api_url = $is_production ?
   }
 
   $request_body = file_get_contents('php://input');
-  header('content-type: application/json');
+  header('Content-Type: application/json');
 
   $charge_result = chargeAPI($api_url,$server_key,$request_body);
 
   http_response_code($charge_result['http_code']);
   echo $charge_result['body'];
 
-  function chargeAPI($request_body,$server_key,$request_body){
+  function chargeAPI($api_url,$server_key,$request_body){
       $ch = curl_init();
       $curl_options =array(
           CURLOPT_URL => $api_url,
@@ -40,7 +40,8 @@ $api_url = $is_production ?
          ),
          CURLOPT_POSTFIELDS => $request_body
         );
-        curl_setopt_array(
+        curl_setopt_array($ch,$curl_options);
+        $result = array(
             'body'=>curl_exec($ch),
             'http_code'=>curl_getinfo(Sch,CURLINFO_HTTP_CODE),
 
@@ -48,6 +49,6 @@ $api_url = $is_production ?
         );
         return $result;
 
-
+   
   }
    ?>
